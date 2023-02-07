d9887c824d551ae6c8484a41de6d379dba86190b
Moffi-bit <lorenzostiavelli6@gmail.com>
2023-02-07 16:35:24 -0500

    Created a generator class and added more functionality.

--- generate.py
+++ generate.py
@@ -1,8 +1,46 @@
-def main() {
-    option = input(int("1. Generate a random chess position\n2. Generate a specific chess position (e.g )"))
-}
+import pandas as pd
+import requests
+import io
+import random
 
-def 
+class Generator():
+    def __init__(self, urlToCSV):
+        self.dataValues = self.loadCSV(urlToCSV)
+    
+    def loadCSV(self, url):
+        download = requests.get(url).content
+
+        dataFrame = pd.read_csv(io.StringIO(download.decode('utf-8')))
+        return dataFrame.values
+    
+    def getRandomOpening(self):
+        return self.dataValues[random.randint(0, len(self.dataValues))]
+
+    def getSpecificOpening(self, specificOpening):
+        if isinstance(specificOpening, str):
+            for opening in self.dataValues:
+                if opening[1].lower().__contains__(specificOpening.lower()):
+                    return f"The moves for the {opening[1]} are {opening[2]}"
+            return f"No opening named {specificOpening} was found."
+        else: 
+            return f"Invalid parameter passed. Expecting type str but received {type(specificOpening)}"
+
+    def getRandomFEN(self):
+        print("TODO")
+
+    def getSpecificFEN(self):
+        print("TODO")
+
+def testCode():
+    gen = Generator("https://raw.githubusercontent.com/tomgp/chess-canvas/master/pgn/chess_openings.csv")
+    randomOpening = gen.getRandomOpening()
+    print(f"The random opening selected is the {randomOpening[1]}.\nThe opening's ECO is {randomOpening[0]}.\nThe opening's moves is {randomOpening[2]}")
+    specificOpening = "Alekhine"
+    specificOpeningMoves = gen.getSpecificOpening(specificOpening)
+    print(f"{specificOpeningMoves}")
+    
+def main():
+    testCode()
 
 if __name__ == '__main__':
     main()
